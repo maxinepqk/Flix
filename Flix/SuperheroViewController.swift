3dc8ce1e825318fb36ad5173d7ec4fb09b361be4
Maxine Kwan <maxinek@fb.com>
2017-06-23 15:00:19 -0700

    Added refresh control, activity indicator and network alert to collection view

--- Flix/SuperheroViewController.swift
+++ Flix/SuperheroViewController.swift
@@ -10,12 +10,34 @@ import UIKit
 
 class SuperheroViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var collectionView: UICollectionView!
+    var refreshControl: UIRefreshControl!
     var movies: [[String: Any]] = []
+    var networkAlertController: UIAlertController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        networkAlertController = UIAlertController(title: "Cannot Get Movies", message: "The Internet connection appears to be offline", preferredStyle: .alert)
+        // create an try again action
+        let tryAgainAction = UIAlertAction(title: "Try Again", style: .default) { (action) in
+            self.newNetworkRequest()
+        }
+        // add the try again action to the alert controller
+        networkAlertController.addAction(tryAgainAction)
+        
         collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        // Start the activity indicator
+        activityIndicator.startAnimating()
+        
+        // Initialize a UIRefreshControl
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        collectionView.insertSubview(refreshControl, at: 0)
+        
         newNetworkRequest()
 
         // Do any additional setup after loading the view.
@@ -30,22 +52,28 @@ class SuperheroViewController: UIViewController, UICollectionViewDataSource, UIC
             // This will run when the network request returns
             if let error = error {
                 // Network Alert Controller
-                //self.present(self.networkAlertController, animated: true)
+                self.present(self.networkAlertController, animated: true)
                 print(error.localizedDescription)
             } else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 let movies = dataDictionary["results"] as! [[String:Any]]
                 self.movies = movies
                 self.collectionView.reloadData()
-                //self.refreshControl.endRefreshing()
+                self.refreshControl.endRefreshing()
             }
-            //self.activityIndicator.stopAnimating()
+            self.activityIndicator.stopAnimating()
         }
         task.resume()
         
     }
     
-
+    // Makes a network request to get updated data
+    // Updates the tableView with the new data
+    // Hides the RefreshControl
+    func refreshControlAction(_ refreshControl: UIRefreshControl) {
+        newNetworkRequest()
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
          return movies.count
     }
